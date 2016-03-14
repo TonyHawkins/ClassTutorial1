dc376341cb53e3d321d6ed9c181937b0efc4d643
Tony Hawkins <Tony-Hawkins@live.nmit.ac.nz>
2016-03-14 14:14:02 +1300

    Changed photo class and form

--- Version 1 C/clsPhotograph.cs
+++ Version 1 C/clsPhotograph.cs
@@ -6,13 +6,37 @@ namespace Version_1_C
     [Serializable()]
     public class clsPhotograph : clsWork
     {
-        private float theWidth;
+        private float _width;
         private float theHeight;
         private string theType;
 
+        [NonSerialized()]
+        private static frmPhotograph photoDialog;
+
+        public float Width
+        {
+            get
+            {
+                return _width;
+            }
+
+            set
+            {
+                _width = value;
+            }
+        }
+
         public override void EditDetails()
         {
-  
+            if (photoDialog == null)
+            {
+                photoDialog = new frmPhotograph();
+            }
+            photoDialog.SetDetails(_Name, theDate, theValue, _width, theHeight, theType);
+            if (photoDialog.ShowDialog() == DialogResult.OK)
+            {
+                photoDialog.GetDetails(ref _Name, ref theDate, ref theValue, ref _width, ref theHeight, ref theType);
+            }
         }
     }
 }
